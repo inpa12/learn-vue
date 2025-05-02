e3d1848bba63d3a16806e881b24d123573a6a6a6
inpa12 <inpa12@gmail.com>
2025-05-02 14:38:41 +0900

    20250502-2

--- src/components/TheView.vue
+++ src/components/TheView.vue
@@ -1,6 +1,10 @@
 <template>
     <main>
         <div class="container py-4">
+            <PostCreate @create-post="createPost"></PostCreate>
+
+            <hr class="my-4" />
+
             <div class="row g-3">
                 <div v-for="post in posts" :key="post.id" class="col col-4">
                     <AppCard
@@ -13,17 +17,37 @@
                     <button @click="post.isLike = !post.isLike">toggle</button>
                 </div>
             </div>
+
+            <hr class="my-4" />
+
+            <!-- <label>
+                이름 :
+                <input v-model="username" type="text" />
+            </label> -->
+            <!-- :model-value="username"
+                @update:model-value="(value) => username.value" -->
+            <LabelInput v-model="username" label="이름"></LabelInput>
+            <LabelTitle v-model:title="username" label="제목"></LabelTitle>
+            <Username v-model:firstname="firstname" v-model:lastname="lastname"></Username>
         </div>
     </main>
 </template>
 
 <script>
 import AppCard from '@/components/AppCard.vue'
-import { reactive } from 'vue'
+import PostCreate from '@/components/PostCreate.vue'
+import LabelInput from '@/components/LabelInput.vue'
+import LabelTitle from '@/components/LabelTitle.vue'
+import Username from '@/components/Username.vue'
+import { reactive, ref } from 'vue'
 
 export default {
     components: {
         AppCard,
+        PostCreate,
+        LabelInput,
+        LabelTitle,
+        Username,
     },
     setup() {
         const post = reactive({
@@ -38,9 +62,21 @@ export default {
             { id: 5, title: '제목5', contents: '내용5', isLike: false, type: 'notice' },
             { id: 6, title: '제목6', contents: '내용6', isLike: false, type: 'news' },
         ])
+        const createPost = (newPost) => {
+            console.log('createPost')
+            console.log('newPost: ', newPost)
+            posts.push(newPost)
+        }
+        const username = ref('')
+        const firstname = ref('')
+        const lastname = ref('')
         return {
             post,
             posts,
+            createPost,
+            username,
+            firstname,
+            lastname,
         }
     },
 }
